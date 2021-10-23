be8b73d696c9271625775163a6301fc2f52a4250
MICHAEL KINGS <57077025+MycoMK@users.noreply.github.com>
2021-10-23 00:10:22 -0700

    edits

--- scritp.js
+++ scritp.js
@@ -1,11 +1,14 @@
-console.log('connected')
+// console.log('connected')
 
 const KELVIN = 273;
 const key = "82005d27a116c2880c8f0fcb866998a0";
 function get(latitude, longitude){
-let api= `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+const api= `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
 }
 
-fetch('api')
-  .then(response => response.json())
-  .then(data => console.log(data));
+fetch('api.json')
+  .then((res) => res.json())
+  .then((data) => {
+    const output =  "<h1>HELLO WORLD</h1>"
+    console.log(data)
+  });
