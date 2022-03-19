eceab896f429f068caf53611d16c20835df90c17
MICHAEL KINGS <57077025+MycoMK@users.noreply.github.com>
2022-03-18 17:43:06 -0700

    api

--- scritp.js
+++ scritp.js
@@ -2,13 +2,17 @@
 
 
 
-const KELVIN = 273;
-const key = "82005d27a116c2880c8f0fcb866998a0";
-function get(latitude, longitude){
-const api= `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
-}
+// const KELVIN = 273;
+// const key = "82005d27a116c2880c8f0fcb866998a0";
+// function get(latitude, longitude){
+// const api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+// }
 
-fetch('api.json')
-  .then((res) => res.json())
-  .then(data => console.log(data))
+// fetch('api.json')
+//   .then((res) => res.json())
+//   .then(data => console.log(data))
   
+
+  fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  .then(json => console.log(json))
