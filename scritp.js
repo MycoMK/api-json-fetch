cc9bb1e8864914c8a389e8a78180ea7c93f683b5
MICHAEL KINGS <57077025+MycoMK@users.noreply.github.com>
2022-05-12 14:59:00 -0700

    Practice

--- scritp.js
+++ scritp.js
@@ -18,6 +18,44 @@ const api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=
   // .then(json => console.log(json))
   
 
-const age = 16;
-const  isOldEnough = age >= 18;
-console.log(isOldEnough)
+// const age = 16;
+// const  isOldEnough = age >= 18;
+// console.log(isOldEnough)
+
+// const inputYear = "1991"
+// console.log (Number(inputYear) + 18)
+
+// const money = 100;
+// if(money){
+//   console.log("Don't spend it all")
+// }else{
+//   console.log("Go get  JOB")
+// }
+
+const favourite = Number(prompt("Whats your favourite number?"));
+console.log(favourite)
+if(favourite === 23){
+  console.log('Cool! 23 is an amazing number!')
+} else if(favourite === 7){
+  console.log(' 7 is also a cool number')
+}else if(favourite === 9){
+  console.log('9 is also a cool number')
+}else{
+  console.log('Number is not 23 , 7 or 9')
+}
+
+
+
+
+// Traffic ligths App
+// const ligths = "Yellow"
+// if(ligths  === "Green") {
+//   console.log('Go')
+// } else if(ligths === "Yellow") {
+//   console.log('Get ready to stop')
+// }
+// else if(ligths === "Red");{
+//   console.log('Stop ')
+// // }else{
+// //   console.log('None')
+// // }
